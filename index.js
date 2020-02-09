c54c420f4b51650ad78ad39bdeef7f89362b9753
Manny <manny.pasquale@kanoapps.com>
2020-02-08 22:57:35 -0800

    temp

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 
 app.post('/start', (request, response) => {
+    // Here is the data for our snake etc
     const data = {
         color: '#E66465',
         headType: 'bwc-bonhomme',
         tailType: 'bwc-flake'
     };
-    console.log(response);
+    app.set('my_location', {x:response.body.you.body.x, y: response.body.you.body.y});
+    app.set('my_health', response.body.you.health);
+    app.set('board_size',
+        {
+            height: response.body.board.height,
+            width: response.body.board.width
+        });
+    app.set('food_location',
+        {
+            x: response.body.board.food[0].x,
+            y:response.body.board.food[0].y
+        });
     return response.json(data);
 });
 
 app.post('/move', (request, response) => {
-    console.log(request);
+    console.log('my snakes current postion');
+    console.log(app.get('my_location'));
+    console.log(app.get('my_health'));
     console.log(request.body);
+});
+
+app.post('/ping', (request, response) => {
+    response.sendStatus(200);
 });
 app.listen(app.get('port'), () => {
     console.log('Server listening on port %s', app.get('port'));
