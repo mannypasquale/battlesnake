77ccd742416ea2829aa1037fca915eecda139ab6
Manny <manny.pasquale@kanoapps.com>
2020-02-08 16:56:57 -0800

    whaddup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 
-app.set('port', (process.env.PORT || 3001))
+app.set('port', (process.env.PORT || 9001))
 app.enable('verbose erros');
 
 app.use(logger('dev'));
