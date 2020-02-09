5488f9ce721c85e694c54c319859209564c18c0a
Manny <manny.pasquale@kanoapps.com>
2020-02-08 16:48:35 -0800

    removed heroku lib

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const logger = require('morgan');
 const app = express();
-const port = 3000;
 
 app.set('port', (process.env.PORT || 3001))
 app.enable('verbose erros');
@@ -12,8 +11,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 
 app.post('/start', (request, response) => {
-    console.log('hitting the start post request');
-    console.log(request.body);
     const data = {
         color: '#E66465',
         headType: 'bwc-bonhomme',
